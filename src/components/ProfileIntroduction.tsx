f0d05a852a0029ec63896078dea4a80cda8ad2a8
yluoc <lyc5dshhh@gmail.com>
2025-10-19 23:50:14 -0400

    update self-into

--- src/components/ProfileIntroduction.tsx
+++ src/components/ProfileIntroduction.tsx
@@ -5,7 +5,9 @@ const ProfileIntroduction: React.FC = () => {
     <div className="profileIntroduction">
       <span>
         I am Yicheng Luo, currently I am working at a startup company as Blockchain Engineer,
-        focusing on solving the real-problem with blockchain technology.
+        focusing on developing web3 dapps and blockchain solutions.
+        Besides, I am interested in C++ development,
+        especially in C++ development for quantitative finance and high-concurrency applications.
       </span>
     </div>
   );
