56c31a30630fbce4b90df73f5bcd6139ba8c0c1d
yluoc <lyc5dshhh@gmail.com>
2025-10-23 21:36:23 -0400

    update self-intro section

--- src/components/ProfileIntroduction.tsx
+++ src/components/ProfileIntroduction.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const ProfileIntroduction: React.FC = () => {
   return (
     <div className="profileIntroduction">
-      <span>
-        Hello, I am Yicheng, currently I am working at a startup company as a Blockchain Engineer, focusing on developing web3 dapps and blockchain solutions.
-        Besides, I am working as a full-stack engineer with my alumni to build an AI tool to enpower Automating Technical Business.
-        I am interested in DSP(Dyson Sphere Program) and try to build interesting mods for it.
-      </span>
+      <h2>About Me</h2>
+      <div className="introduction-content">
+        <p>
+          Hello, I am Yicheng, currently I am working at a startup company as a Blockchain Engineer, focusing on developing web3 dapps and blockchain solutions.
+          Besides, I am working as a full-stack engineer with my alumni to build an AI tool to enpower Automating Technical Business.
+          I am interested in DSP(Dyson Sphere Program) and try to build interesting mods for it.
+        </p>
+      </div>
     </div>
   );
 };
